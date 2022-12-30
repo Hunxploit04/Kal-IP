cd78f5f76144c393d32fd9afcfbbb5edc32a9323
HunX04 <121525561+Hunxploit04@users.noreply.github.com>
2022-12-30 14:17:09 +0800

    Update fuv-ip.sh

--- fuv-ip.sh
+++ fuv-ip.sh
@@ -78,7 +78,7 @@ if [ $no = 01 ]
         then
             echo -e $white"Penggunaan : $do IP Address/prefix or 192.X.X.X/26"
             sleep 3
-            ./FuvIP.sh
+            ./fuv-ip.sh
 
         elif [ $no = 00 ]        
             then
@@ -90,7 +90,7 @@ if [ $no = 01 ]
                 echo -e $white"Maaf Nomor menu tidak tersedia !"
                 sleep 2
                 echo -e $cyan"Kembali ke tools..."
-                ./FuvIP.sh
+                ./fuv-ip.sh
 fi
 
 # Dilarang me Record !!
